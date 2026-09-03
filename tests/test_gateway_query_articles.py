4f3df7fc790ed3f456c18e99c799d3cf20529911
voxathon <212505423+voxathon@users.noreply.github.com>
2026-09-03 07:16:23 -0700

    chore: misc update

--- tests/test_gateway_query_articles.py
+++ tests/test_gateway_query_articles.py
@@ -1,3 +1,17 @@
+# Copyright 2026 The Bonnet Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """query_articles: the reply_to and root filters, added alongside the
 threading docstring so an agent can walk a thread one level at a time
 (reply_to) or fetch it all in one call (root). See test_gateway_thread.py
